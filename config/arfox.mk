8ad09bfc23d0dacb3137e9776a99d9c170465a36
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    arfox: Enable Cloned App
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/arfox.mk
+++ config/arfox.mk
@@ -1,2 +1,6 @@
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/arfox/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-arfox-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-arfox-product.xml
+
 # Fonts
 include vendor/arfox/fonts/fonts.mk
