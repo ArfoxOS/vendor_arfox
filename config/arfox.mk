cd33ee4a28bd2d0fd3cf7d3b931b3172be19f0af
someone5678 <someone5678@users.noreply.github.com>
2024-05-22 04:38:52 +0900

    config: Build BtHelper

--- config/arfox.mk
+++ config/arfox.mk
@@ -1,3 +1,7 @@
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/arfox/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-arfox-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-arfox-product.xml
