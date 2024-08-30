dad8ffad27e8bfa50087e936277467d08a1a81da
ShevT <evgeny.shishkov@gmail.com>
2024-08-30 12:38:33 +0300

    config: Adding an option to build DeviceAsWebcam

--- config/arfox.mk
+++ config/arfox.mk
@@ -2,6 +2,15 @@
 PRODUCT_COPY_FILES += \
     vendor/arfox/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-arfox-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-arfox-product.xml
 
+# DeviceAsWebcam
+ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
+    PRODUCT_PACKAGES += \
+        DeviceAsWebcam
+
+    PRODUCT_VENDOR_PROPERTIES += \
+        ro.usb.uvc.enabled=true
+endif
+
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
