clarencekopitiam/android_vendor_lineage

vendor: Set Google Sans Text as the default fonts
Co-authored-by: Anay Wadhera <anay1018@gmail.com>
Change-Id: I1509e59f9a0a662a7ba8d73148411a1ce8be3991
Signed-off-by: clarencelol <clarencekuiek@proton.me>

config/common.mk
@@ -90,6 +90,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Fonts
+include vendor/lineage/config/fonts.mk
+
 # This is Lineage!
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.android.xml

config/fonts.mk
@@ -1,0 +1,7 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/lineage/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    vendor/lineage/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/lineage/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/lineage/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
+    vendor/lineage/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml

ArfoxOS/vendor_arfox

config/common.mk
@@ -231,6 +231,8 @@ CUSTOM_LOCALES += \
     cy_GB \
     fur_IT
 
+include vendor/arfox/config/arfox.mk
+
 include vendor/arfox/config/version.mk
 
 -include vendor/arfox-priv/keys/keys.mk

fonts/fonts.mk
@@ -1,0 +1,7 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/arfox/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    vendor/arfox/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/arfox/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/arfox/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
+    vendor/arfox/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml