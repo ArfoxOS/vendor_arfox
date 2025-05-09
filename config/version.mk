The-Pixel-Project/vendor_aosp

config: Add Maintainer prop

config/version.mk
@@ -15,7 +15,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
     ro.custom.version=$(CUSTOM_VERSION) \
     ro.custom.display.version=$(CUSTOM_DISPLAY_VERSION) \
-    ro.custom.releasetype=$(CUSTOM_BUILD_TYPE)
+    ro.custom.releasetype=$(CUSTOM_BUILD_TYPE) \
+    ro.custom.maintainer=$(CUSTOM_MAINTAINER)
 
 # Only include Updater for official  build
 ifeq ($(filter-out OFFICIAL,$(CUSTOM_BUILD_TYPE)),)

ArfoxOS/vendor_arfox

config/version.mk
@@ -16,6 +16,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.arfox.version=$(ARFOX_VERSION) \
     ro.arfox.display.version=$(ARFOX_DISPLAY_VERSION) \
     ro.arfox.releasetype=$(ARFOX_BUILD_TYPE) \
+    ro.arfox.maintainer=$(ARFOX_MAINTAINER)
 
 # Only include Updater for official  build
 ifeq ($(filter-out OFFICIAL,$(ARFOX_BUILD_TYPE)),)